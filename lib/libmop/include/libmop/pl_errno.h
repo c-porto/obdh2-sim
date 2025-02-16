6fd6a7321947dbc90fcbb6c3a9b7fe9e7a2c737e
Carlos Augusto Porto Freitas <carlos.portof@hotmail.com>
2025-02-15 21:11:16 -0300

    lib: Add libmop initial version

---
+++ lib/libmop/include/libmop/pl_errno.h
@@ -0,0 +1,15 @@
+#ifndef PL_ERRNO_H_
+#define PL_ERRNO_H_
+
+#include <stdint.h>
+
+enum pl_errno {
+	PL_OK = 0,
+	PL_ERRNO_INVALID_ARG = 1,
+	PL_ERRNO_TIMEOUT = 2,
+	PL_ERRNO_UNKNOWN = 3,
+	PL_ERRNO_UNSUPPORTED_FN = 4,
+	PL_ERRNO_IO = 5,
+};
+
+#endif
---
+++ lib/libmop/src/payload.c
@@ -0,0 +1,86 @@
+#include <libmop/pl_errno.h>
+#include <libmop/payload.h>
+#include <stdint.h>
+
+int payload_init(struct payload *pl)
+{
+	int err = PL_ERRNO_INVALID_ARG;
+
+	if ((pl != NULL) && (pl->init != NULL))
+		err = (pl->init)(pl);
+
+	return err;
+}
+
+int payload_write_data(struct payload *pl, uint8_t type, uint8_t *data,
+		       uint16_t size)
+{
+	int err = PL_ERRNO_INVALID_ARG;
+
+	if ((pl != NULL) && (pl->write_data != NULL) && (data != NULL))
+		err = (pl->write_data)(pl, type, data, size);
+
+	return err;
+}
+
+int payload_read_data(struct payload *pl, uint8_t type, uint8_t *data,
+		      uint16_t size)
+{
+	int err = PL_ERRNO_INVALID_ARG;
+
+	if ((pl != NULL) && (pl->read_data != NULL) && (data != NULL))
+		err = (pl->read_data)(pl, type, data, size);
+
+	return err;
+}
+
+int payload_write_cmd(struct payload *pl, uint8_t cmd, uint8_t *cmd_args,
+		      uint16_t args_size)
+{
+	int err = PL_ERRNO_INVALID_ARG;
+
+	if ((pl != NULL) && (pl->write_cmd != NULL) && (cmd_args != NULL))
+		err = (pl->write_cmd)(pl, cmd, cmd_args, args_size);
+
+	return err;
+}
+
+int payload_set_clock(struct payload *pl, struct payload_timestamp *ts)
+{
+	int err = PL_ERRNO_INVALID_ARG;
+
+	if ((pl != NULL) && (pl->set_clock != NULL) && (ts != NULL))
+		err = (pl->set_clock)(pl, ts);
+
+	return err;
+}
+
+int payload_get_clock(struct payload *pl, struct payload_timestamp *ts)
+{
+	int err = PL_ERRNO_INVALID_ARG;
+
+	if ((pl != NULL) && (pl->get_clock != NULL) && (ts != NULL))
+		err = (pl->get_clock)(pl, ts);
+
+	return err;
+}
+
+int payload_enable(struct payload *pl)
+{
+	int err = PL_ERRNO_INVALID_ARG;
+
+	if ((pl != NULL) && (pl->enable != NULL))
+		err = (pl->enable)(pl);
+
+	return err;
+}
+
+int payload_disable(struct payload *pl)
+{
+	int err = PL_ERRNO_INVALID_ARG;
+
+	if ((pl != NULL) && (pl->disable != NULL))
+		err = (pl->disable)(pl);
+
+	return err;
+}
