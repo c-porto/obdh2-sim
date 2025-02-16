cd4d8d78b25c04da2a916f9ea31157c35d03ada8
Carlos Augusto Porto Freitas <carlos.portof@hotmail.com>
2025-02-16 15:29:48 -0300

    drivers: Add EDC driver files from OBDH2

---
+++ src/drivers/edc_gpio.c
@@ -0,0 +1,55 @@
+/*
+ * edc_gpio.c
+ * 
+ * Copyright (C) 2021, SpaceLab.
+ * 
+ * This file is part of OBDH 2.0.
+ * 
+ * OBDH 2.0 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * OBDH 2.0 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with OBDH 2.0. If not, see <http:/\/www.gnu.org/licenses/>.
+ * 
+ */
+
+/**
+ * \brief EDC driver GPIO routines implementation.
+ * 
+ * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
+ * 
+ * \version 0.8.15
+ * 
+ * \date 2021/10/16
+ * 
+ * \addtogroup edc
+ * \{
+ */
+
+#include <drivers/edc.h>
+
+/* TODO: Unsupported */
+
+int edc_gpio_init(edc_config_t config)
+{
+    return 0;
+}
+
+int edc_gpio_set(edc_config_t config)
+{
+    return 0;
+}
+
+int edc_gpio_clear(edc_config_t config)
+{
+    return 0;
+}
+
+/** \} End of edc group */
