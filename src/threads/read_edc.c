af8de324c3758e9dfa2afc839be2a3110511f455
Carlos Augusto Porto Freitas <carlos.portof@hotmail.com>
2026-01-12 00:22:51 -0300

    threads: read_edc: Fix thread loop and add orbit aware pl control

--- src/threads/read_edc.c
+++ src/threads/read_edc.c
@@ -2,7 +2,9 @@
 #include <math.h>
 
 #include <libmop/payload.h>
+#include <libmop/pl_errno.h>
 
+#include <system/context.h>
 #include <system/sys_log.h>
 #include <system/db.h>
 #include <devices/payload.h>
@@ -56,9 +58,10 @@ static void edc_save_ptt(struct db_handle *db, struct payload *edc,
 
 void *read_edc_thread(void *arg)
 {
-	(void)arg;
+	struct obdh_sim_ctx *ctx = arg;
 
 	struct timespec next = { 0 };
+	struct timespec now = { 0 };
 	struct payload edc = { 0 };
 	struct payload_ctx edc_ctx = { 0 };
 	edc_config_t edc_conf;
@@ -72,6 +75,8 @@ void *read_edc_thread(void *arg)
 	if (create_tm_db(&db, db_file) < 0)
 		sys_log_print_event_from_module(SYS_LOG_ERROR, "edc",
 						"Failed to create DB!");
+
+	edc_conf.en_pin = 0U;
 
 	if (payload_edc_init(1U, &edc, &edc_conf, &edc_ctx) != 0) {
 		sys_log_print_event_from_module(
@@ -90,62 +95,101 @@ void *read_edc_thread(void *arg)
 	for (;;) {
 		next.tv_sec += 60;
 
-		struct payload_timestamp ts = {
-			.tv_sec = next.tv_sec - 60U,
-			.tv_nsec = next.tv_nsec,
-		};
+		if (!ctx->cond.in_brazil) {
+			if (edc.ctx->active) {
+				sys_log_print_event_from_module(
+					SYS_LOG_INFO, "edc",
+					"Disabling EDC...");
 
-		if (payload_set_clock(&edc, &ts) != 0) {
-			sys_log_print_event_from_module(SYS_LOG_ERROR, "edc",
-							"Failed to set clock!");
-		}
-
-		uint8_t cmd[4] = { 0 };
-		if (payload_write_cmd(&edc, EDC_CMD_PTT_RESUME, cmd,
-				      sizeof(cmd)) != 0) {
-			sys_log_print_event_from_module(
-				SYS_LOG_ERROR, "edc",
-				"Failed to resume ptt task!");
-		}
-
-		edc_delay_ms(50U);
-
-		if (payload_read_data(&edc, EDC_FRAME_ID_HK, (uint8_t *)&hk,
-				      sizeof(hk)) == 0) {
-			edc_save_hk(&db, &edc, &hk);
-		} else {
-			sys_log_print_event_from_module(SYS_LOG_ERROR, edc.name,
-							"Failed to read hk!");
-		}
-
-		edc_delay_ms(500U);
-
-		if (payload_read_data(&edc, EDC_FRAME_ID_STATE,
-				      (uint8_t *)&state, sizeof(state)) == 0) {
-			edc_save_state(&db, &edc, &state);
-
-			if (state.ptt_available > 0) {
-				for (uint8_t i = 0; i < state.ptt_available;
-				     i++) {
-					if (payload_read_data(
-						    &edc, EDC_FRAME_ID_PTT,
-						    (uint8_t *)&ptt,
-						    sizeof(ptt)) == 0) {
-						edc_save_ptt(&db, &edc, &ptt);
-					} else {
-						sys_log_print_event_from_module(
-							SYS_LOG_ERROR, edc.name,
-							"Error reading ptt package!");
-					}
-
-					edc_delay_ms(500U);
+				if (payload_disable(&edc) != PL_OK) {
+					sys_log_print_event_from_module(
+						SYS_LOG_ERROR, "edc",
+						"Failed to disabled edc!");
 				}
 			}
-		} else {
-			sys_log_print_event_from_module(SYS_LOG_ERROR, edc.name,
-							"Error reading state!");
 		}
 
-		return NULL;
+		if (ctx->cond.in_brazil) {
+			if (!edc.ctx->active) {
+				sys_log_print_event_from_module(
+					SYS_LOG_INFO, "edc", "Enabling EDC...");
+
+				if (payload_enable(&edc) != PL_OK) {
+					sys_log_print_event_from_module(
+						SYS_LOG_ERROR, "edc",
+						"Failed to enable edc!");
+				}
+			}
+
+			clock_gettime(CLOCK_MONOTONIC, &now);
+
+			struct payload_timestamp ts = {
+				.tv_sec = now.tv_sec,
+				.tv_nsec = now.tv_nsec,
+			};
+
+			if (payload_set_clock(&edc, &ts) != 0) {
+				sys_log_print_event_from_module(
+					SYS_LOG_ERROR, "edc",
+					"Failed to set clock!");
+			}
+
+			uint8_t cmd[4] = { 0 };
+			if (payload_write_cmd(&edc, EDC_CMD_PTT_RESUME, cmd,
+					      sizeof(cmd)) != 0) {
+				sys_log_print_event_from_module(
+					SYS_LOG_ERROR, "edc",
+					"Failed to resume ptt task!");
+			}
+
+			edc_delay_ms(50U);
+
+			if (payload_read_data(&edc, EDC_FRAME_ID_HK,
+					      (uint8_t *)&hk,
+					      sizeof(hk)) == 0) {
+				edc_save_hk(&db, &edc, &hk);
+			} else {
+				sys_log_print_event_from_module(
+					SYS_LOG_ERROR, edc.name,
+					"Failed to read hk!");
+			}
+
+			edc_delay_ms(500U);
+
+			if (payload_read_data(&edc, EDC_FRAME_ID_STATE,
+					      (uint8_t *)&state,
+					      sizeof(state)) == 0) {
+				edc_save_state(&db, &edc, &state);
+
+				if (state.ptt_available > 0) {
+					for (uint8_t i = 0;
+					     i < state.ptt_available; i++) {
+						if (payload_read_data(
+							    &edc,
+							    EDC_FRAME_ID_PTT,
+							    (uint8_t *)&ptt,
+							    sizeof(ptt)) == 0) {
+							edc_save_ptt(&db, &edc,
+								     &ptt);
+						} else {
+							sys_log_print_event_from_module(
+								SYS_LOG_ERROR,
+								edc.name,
+								"Error reading ptt package!");
+						}
+
+						edc_delay_ms(500U);
+					}
+				}
+			} else {
+				sys_log_print_event_from_module(
+					SYS_LOG_ERROR, edc.name,
+					"Error reading state!");
+			}
+		}
+
+		clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &next, NULL);
 	}
+
+	return NULL;
 }
